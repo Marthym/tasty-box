b08622fc8d5208eee0afffd111a95b91bc4efb34
Marthym <marthym@gmail.com>
2024-02-03 17:09:18 +0100

    feat(mongo): #10 add dbTracker to WithMongoData
    close #10

--- testy-mongo-box/src/test/java/fr/ght1pc9kc/testy/mongo/WithMongoDataTest.java
+++ testy-mongo-box/src/test/java/fr/ght1pc9kc/testy/mongo/WithMongoDataTest.java
@@ -7,7 +7,10 @@ import fr.ght1pc9kc.testy.mongo.sample.ClazzDataSet;
 import fr.ght1pc9kc.testy.mongo.sample.DocumentDataSet;
 import org.bson.Document;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -18,16 +21,15 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class WithMongoDataTest {
 
     private static final String COLLECTION_0 = "firstCollection";
     private static final String COLLECTION_1 = "secondCollection";
     private static final String COLLECTION_2 = "dummyCollection";
 
-    private static final WithEmbeddedMongo WITH_EMBEDDED_MONGO = WithEmbeddedMongo.builder()
-            .build();
-    private static final WithObjectMapper WITH_OBJECT_MAPPER = WithObjectMapper.builder()
-            .build();
+    private static final WithEmbeddedMongo WITH_EMBEDDED_MONGO = WithEmbeddedMongo.builder().build();
+    private static final WithObjectMapper WITH_OBJECT_MAPPER = WithObjectMapper.builder().build();
     private static final WithMongoData WITH_MONGO_DATA = WithMongoData.builder(WITH_EMBEDDED_MONGO)
             .withObjectMapper(WITH_OBJECT_MAPPER)
             .addDataset(COLLECTION_0, new DocumentDataSet())
@@ -49,6 +51,7 @@ class WithMongoDataTest {
         this.mongoTemplate = mongoTemplate;
     }
 
+    @Order(1)
     @ParameterizedTest
     @ValueSource(strings = {
             COLLECTION_0,
@@ -66,7 +69,10 @@ class WithMongoDataTest {
     }
 
     @Test
-    void should_have_inserted_clazz_data() {
+    @Order(2)
+    void should_have_inserted_clazz_data(WithMongoData.Tracker tracker) {
+        tracker.skipNextSampleLoad();
+
         final List<Document> actual = mongoTemplate.findAll(Document.class, COLLECTION_2)
                 .collectList()
                 .block();
@@ -74,5 +80,21 @@ class WithMongoDataTest {
         assertThat(actual).containsExactly(
                 new Document(Map.of("_id", "Luke", "bar", "Skywalker")),
                 new Document(Map.of("_id", "Obiwan", "bar", "Kenobi")));
+
+        mongoTemplate.insert(new Document(Map.of("_id", "Darth", "name", "Vader")), COLLECTION_2)
+                .block();
+    }
+
+    @Test
+    @Order(3)
+    void should_skip_reset_data() {
+        final List<Document> actual = mongoTemplate.findAll(Document.class, COLLECTION_2)
+                .collectList()
+                .block();
+
+        assertThat(actual).containsExactly(
+                new Document(Map.of("_id", "Luke", "bar", "Skywalker")),
+                new Document(Map.of("_id", "Obiwan", "bar", "Kenobi")),
+                new Document(Map.of("_id", "Darth", "name", "Vader")));
     }
 }
